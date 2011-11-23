b7ae61fea658f36d6e47a7e8a82e23f3e123e54c
Nickolay Kudasov <crazy.fizruk@gmail.com>
2011-11-23 05:05:08 +0400

    gui: use glade file

--- src/Main.hs
+++ src/Main.hs
@@ -1,55 +1,18 @@
 module Main where
 
 import Graphics.UI.Gtk
+import Graphics.UI.Gtk.Glade
+
+import Paths_DiffGame (getDataFileName)
 
 main :: IO ()
 main = do
     initGUI
-    window    <- windowNew
 
-    -- creating box for the whole interface
-    ifBox <- vBoxNew False 10
-    
-    -- creating frame for constants
-    constFrame <- frameNew
-    frameSetLabel constFrame "Определение констант"
+    gladeFile   <- getDataFileName "gui/MainWindow.glade"
+    Just xml    <- xmlNew gladeFile
+    window      <- xmlGetWidget xml castToWindow "window1"
 
-    constBox <- hBoxNew False 10
-    dimAdj   <- adjustmentNew 1 1 10 1 1 0
-    dimNum   <- spinButtonNew dimAdj 1 0
-    boxPackStart constBox dimNum PackGrow 10
-    containerAdd constFrame constBox
-
-    -- creating box for initial position and function definitions
-    varBox <- hBoxNew False 10
-
-    -- creating frame for initial position definition
-    posFrame <- frameNew
-    frameSetLabel posFrame "Начальное положение"
-
-    posBox  <- vBoxNew True 10
-    pos1    <- entryNew
-    entrySetText pos1 "0"
-    boxPackStart posBox pos1 PackGrow 10
-    containerAdd posFrame posBox
-
-    -- creating frame for RHS definition
-    rhsFrame <- frameNew
-    frameSetLabel rhsFrame "Определение правой части"
-
-    rhsBox   <- vBoxNew True 10
-    rhs1     <- entryNew
-    entrySetText rhs1 "0"
-    boxPackStart rhsBox rhs1 PackGrow 10
-    containerAdd rhsFrame rhsBox
-
-    boxPackStart varBox posFrame PackGrow 10
-    boxPackStart varBox rhsFrame PackGrow 10
-
-    boxPackStart ifBox constFrame PackGrow 10
-    boxPackStart ifBox varBox PackGrow 0
-    
-    containerAdd window ifBox
     onDestroy window mainQuit
     widgetShowAll window
     mainGUI
